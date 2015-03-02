2628c673fd969f7b170d1a58420444519c06ec13
Bjørn Uhre Arnholtz <bjorn@bjornhub.com>
2015-03-02 21:33:33 +0100

    Started working on network

--- src/js/dispatcher.js
+++ src/js/dispatcher.js
@@ -5,14 +5,7 @@ var coldstorage = require("coldstorage");
 module.exports = coldstorage.createDispatcher([
     require("./store/navigation.js"),
     require("./store/sidemenu.js"),
+    require("./store/network.js"),
     require("./store/user.js"),
     require("./store/menu.js")
 ]);
-
-
-// Dispatchr.registerStore(require('./store/navigation.js'));
-// Dispatchr.registerStore(require('./store/title.js'));
-// Dispatchr.registerStore(require('./store/user.js'));
-// Dispatchr.registerStore(require('./store/messages.js'));
-// Dispatchr.registerStore(require('./store/sidemenu.js'));
-// module.exports = Dispatchr;
--- src/js/store/network.js
+++ src/js/store/network.js
@@ -1,26 +1,20 @@
-/*global module, require, console */
-var createStore = require("dispatchr/utils/createStore");
+"use strict";
 
-module.exports = createStore({
-    storeName: "user",
-    initialize: function () {
-        "use strict";
-        this.isAuthed = false;
-    },
-    handlers: {
-        signin: function (payload) {
-            "use strict";
-            var self = this;
-            setTimeout(function () {
-                self.username = payload.username;
-                self.isAuthed = true;
-                self.emitChange();
-            }, 1000);
-        },
-        signout: function () {
-            "use strict";
-            this.isAuthed = false;
-            this.emitChange();
+var coldstorage = require("coldstorage");
+var action = require("../action");
+var Immutable = require("immutable");
+
+var store = coldstorage.createStore({
+    id: "network",
+    update: function (old, get) {
+        if (get(action.init) !== undefined) {
+            return Immutable.fromJS({
+                waiting: {},
+                ready: {}
+            });
         }
+        return old;
     }
 });
+
+module.exports = store;
