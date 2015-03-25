394d82b207c95a4ce60e77cdb76083b8c2bd6ab2
bjornua <bjorn.arnholtz@gmail.com>
2015-03-25 20:24:53 +0100

    arghh

--- src/js/store/async.js
+++ src/js/store/async.js
@@ -13,41 +13,42 @@ var init = Immutable.fromJS({
     ready: {}
 });
 
-var onNavigate = function (get) {
-    var handler = get(Navigation).get("handler");
-    var queued = queued;
-    var res = Immutable.fromJS(handler.async(get));
-    if (handler.async !== undefined) {
-        var id = res.get("id");
-        utils.assert(id, "id must be defined in {}", res);
-        res = res.remove("id");
-        return queued.set(id, res);
-    }
-    return Immutable.List();
-};
-
-var store = coldstorage.createStore({
-    id: "async",
-    update: function (old, get) {
+var funcs = [
+    function (state, get) {
         if (get(action.init) !== undefined) {
             return init;
         }
-        var queued = old.get("queued");
+        return state;
+    },
+    function (state, get) {
+        var queued = state.get("queued");
         if (get(action.setURL) !== undefined || get(action.setView) !== undefined) {
-
+            var handler = get(Navigation).get("handler");
+            var res = Immutable.fromJS(handler.async(get));
+            if (handler.async !== undefined) {
+                var id = res.get("id");
+                utils.assert(id, "id must be defined in {}", res);
+                res = res.remove("id");
+                queued.set(id, res);
+            }
         }
-
-        old = old.update("queued", function (oldwaiting) {
-            return queued.concat(oldwaiting);
-        });
-
+        return state;
+    },
+    function (state, get) {
         if (get(action.asyncStart) !== undefined) {
             var items = get(action.asyncStart).get("items");
-            queued = old.get("queued");
-            
+            queued = state.get("queued");
         }
+        return state;
+    }
+];
 
-        return old;
+var store = coldstorage.createStore({
+    id: "async",
+    update: function (state, get) {
+
+
+        return state;
     }
 });
 
