2e95cad2330975178948af851e30c0275bfdf865
Bjørn Uhre Arnholtz <bjorn@bjornhub.com>
2015-03-25 21:07:21 +0100

    Fix

--- src/js/store/async.js
+++ src/js/store/async.js
@@ -7,34 +7,37 @@ var Immutable = require("immutable");
 var utils = require("../lib/utils");
 
 
-var init = Immutable.fromJS({
-    queued: {},
-    waiting: {},
-    ready: {}
-});
-
 var onNavigate = function (get) {
-    var handler = get(Navigation).get("handler");
-    var queued = queued;
-    var res = Immutable.fromJS(handler.async(get));
-    if (handler.async !== undefined) {
-        var id = res.get("id");
-        utils.assert(id, "id must be defined in {}", res);
-        res = res.remove("id");
-        return queued.set(id, res);
-    }
-    return Immutable.List();
+
 };
 
 var store = coldstorage.createStore({
     id: "async",
-    update: function (old, get) {
-        if (get(action.init) !== undefined) {
-            return init;
+    init: function () {
+        return {
+            queued: {},
+            waiting: {},
+            ready: {}
+        };
+    },
+    asyncStart: function (old, get) {
+    },
+    _onNavigate: function(old, get) {
+        var handler = get(Navigation).get("handler");
+        var queued = queued;
+        var res = Immutable.fromJS(handler.async(get));
+        if (handler.async !== undefined) {
+            var id = res.get("id");
+            utils.assert(id, "id must be defined in {}", res);
+            res = res.remove("id");
+            return queued.set(id, res);
         }
+        return Immutable.List();
+    },
+    default: function (old, get) {
         var queued = old.get("queued");
         if (get(action.setURL) !== undefined || get(action.setView) !== undefined) {
-
+            
         }
 
         old = old.update("queued", function (oldwaiting) {
