2fad1ec564764d8f81057dcc1a81a835053b706c
Bjørn Uhre Arnholtz <bjorn@bjornhub.com>
2015-07-25 22:46:56 +0200

    Added stuff

--- src/js/component/views/landing.js
+++ src/js/component/views/landing.js
@@ -6,12 +6,18 @@ import Page from "./wrappers/page";
 
 class Component extends React.Component {
     render() {
-        return <Page title="Landing page">
+        return <Page title="Landing page" className="dh-maxw35">
             <div>
-                <h1>Find your cleaner today</h1>
-                <div className="dh-maxw35">
-                    <input type="text" placeholder="What is your address" />
-                </div>
+                <h1>Get your home cleaned today</h1>
+                <form className="dh-maxw35 pure-form pure-form-stacked pure-g">
+                    <fieldset className="pure-group">
+                        <input type="text" placeholder="Zip code" className="pure-input-1"/>
+                        <input type="text" placeholder="City" className="pure-input-1"/>
+                        <input type="text" placeholder="Street" className="pure-input-1"/>
+                        <input type="text" placeholder="Street Number" className="pure-input-1"/>
+                        <input type="submit" value="Find cleaning" className="pure-button pure-button-primary pure-input-1" />
+                    </fieldset>
+                </form>
             </div>
         </Page>;
     }
--- src/js/component/views/wrappers/page.js
+++ src/js/component/views/wrappers/page.js
@@ -5,8 +5,14 @@ import Base from "./base";
 
 export default class Page extends React.Component {
     render() {
+        let className = "dh-mainpage";
+
+        if (this.props.className !== undefined) {
+            className += " " + this.props.className;
+        }
+
         return <Base title={this.props.title}>
-            <div className="dh-mainpage">
+            <div className={className}>
                 {this.props.children}
             </div>
         </Base>;
