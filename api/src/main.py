fbb3027642297d122fe3a1afbc27ac07876e1c5f
Bjørn Uhre Arnholtz <bjorn@bjornhub.com>
2015-08-25 09:17:55 +0200

    fix

--- api/src/main.py
+++ api/src/main.py
@@ -3,19 +3,20 @@ app = Flask(__name__)
 
 
 
+def get_conn():
+    import psycopg2
+    db = psycopg2.connect(user="lindo")
+    g.db = db
+
 
 
 @app.before_first_request
 def setup():
     pass
-    
+
 
 @app.before_request
 def setup_request():
-    import psycopg2
-
-    db = psycopg2.connect(user="lindo")
-    g.db = db
 
 
 
