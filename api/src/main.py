3c4bc3bc57407fcbe18c376369a0aaef593de536
Bjørn Uhre Arnholtz <bjorn@bjornhub.com>
2015-08-25 09:17:36 +0200

    stuff

--- api/src/main.py
+++ api/src/main.py
@@ -1,15 +1,27 @@
-from flask import Flask, request
+from flask import Flask, request, g
 app = Flask(__name__)
 
-import testee
+
+
+
 
 @app.before_first_request
 def setup():
     pass
     
 
+@app.before_request
+def setup_request():
+    import psycopg2
+
+    db = psycopg2.connect(user="lindo")
+    g.db = db
+
+
+
 @app.route('/event/', methods=['GET'])
 def event():
+    db = g.db
     1/0
     return 'Hello woooorld!'
 
@@ -21,5 +33,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-    kjkOHvOWth
